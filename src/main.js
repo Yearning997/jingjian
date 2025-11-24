02b81610741d59f611aa379f84878025c8681c0c
Yearning997 <1597816923@qq.com>
2025-11-24 17:55:16 +0800

    修复白屏问题：配置Vue路由base路径

--- src/main.js
+++ src/main.js
@@ -187,7 +187,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(process.env.NODE_ENV === 'production' ? '/jingjian/' : '/'),
   routes
 })
 
