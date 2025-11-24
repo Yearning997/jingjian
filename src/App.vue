67b2f84eea6455b7d0607cededadd12388443e44
Yearning997 <1597816923@qq.com>
2025-11-24 17:58:13 +0800

    简化测试：移除路由，直接显示简单内容

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-    <router-view />
+    <h1>鲸舰电玩城管理系统</h1>
+    <el-button type="primary">测试按钮</el-button>
+    <p>如果您看到这个页面，说明基础配置是正确的</p>
   </div>
 </template>
 
@@ -16,5 +18,6 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
+  padding: 20px;
 }
 </style>
