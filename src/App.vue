db6dc6989e2f8c791d560628cc83965c44462962
Yearning997 <1597816923@qq.com>
2025-11-24 18:06:16 +0800

    调试版本：移除路由，添加时间显示和console.log

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,26 @@
 <template>
   <div id="app">
-    <router-view />
+    <h1>鲸舰电玩城管理系统</h1>
+    <el-button type="primary">测试按钮</el-button>
+    <p>如果您看到这个页面，说明基础配置是正确的</p>
+    <p>现在时间是: {{ currentTime }}</p>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data() {
+    return {
+      currentTime: new Date().toLocaleString()
+    }
+  },
+  mounted() {
+    console.log('Vue应用已挂载')
+    setInterval(() => {
+      this.currentTime = new Date().toLocaleString()
+    }, 1000)
+  }
 }
 </script>
 
@@ -16,5 +30,6 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
+  padding: 20px;
 }
 </style>
